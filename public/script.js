409211cbe771085d93e8551fabf9c8f6c42ecfd1
BİLİ <121384275+baharsevinti@users.noreply.github.com>
2024-04-07 22:11:59 +0300

    init index.html

---
+++ public/script.js
@@ -0,0 +1,9 @@
+const socket=io();
+const messageForm=document.getElementById("mesaj formu");
+const messageInput=document.getElementById("mesaj girdisi");
+const messages=documnet.getElementById("mesaj");
+
+ function displayMessage(role,message){
+    const div=document.createElement("div");
+    div.innerHTML='<p>'
+ }
